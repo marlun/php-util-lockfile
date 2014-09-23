affa96bd225fd6976f6390a75faa91636fd28fea
Martin Lundberg <marlun@mrmac.local>
2014-09-23 17:25:15 +0200

    Changes to a static class function

--- test/LockfileTest.php
+++ test/LockfileTest.php
@@ -15,52 +15,52 @@ class LockfileTests extends PHPUnit_Framework_TestCase {
 	function testSendingInBadFilename() {
 		$this->setExpectedException('InvalidArgumentException',
 			'Lock file path is empty');
-		$result = Problem\Util\lockfile(array('path' => ' '));
+		Problem\Util\LockFile::test(array('path' => ' '));
 	}
 
 	function testSendingInBadMaxCount() {
 		$this->setExpectedException('InvalidArgumentException',
 			'Lock file max count is not numeric');
-		$result = Problem\Util\lockfile(array('maxCount' => 'r'));
+		Problem\Util\LockFile::test(array('maxCount' => 'r'));
 	}
 
 	function testThatLockFileIsCreated() {
 		$this->assertFalse($this->root->hasChild('LOCK_FILE'));
-		Problem\Util\lockfile(array('path' => $this->path));
+		Problem\Util\LockFile::test(array('path' => $this->path));
 		$this->assertTrue($this->root->hasChild('LOCK_FILE'));
 	}
 
 	function testThatCounterDefaultsToOne() {
 		$this->setExpectedException('Exception',
 			'Maximum allowed parallel executions');
-		Problem\Util\lockfile(array('path' => $this->path));
-		Problem\Util\lockfile(array('path' => $this->path));
+		Problem\Util\LockFile::test(array('path' => $this->path));
+		Problem\Util\LockFile::test(array('path' => $this->path));
 	}
 
 	function testThatCounterWorks() {
-		Problem\Util\lockfile(array('path' => $this->path, 'maxCount' => 3));
+		Problem\Util\LockFile::test(array('path' => $this->path, 'maxCount' => 3));
 		$this->assertEquals(1, file_get_contents($this->path));
-		Problem\Util\lockfile(array('path' => $this->path, 'maxCount' => 3));
+		Problem\Util\LockFile::test(array('path' => $this->path, 'maxCount' => 3));
 		$this->assertEquals(2, file_get_contents($this->path));
-		Problem\Util\lockfile(array('path' => $this->path, 'maxCount' => 3));
+		Problem\Util\LockFile::test(array('path' => $this->path, 'maxCount' => 3));
 		$this->assertEquals(3, file_get_contents($this->path));
 	}
 
 	function testThatMaxCountWorks() {
 		$this->setExpectedException('Exception',
 			'Maximum allowed parallel executions');
-		Problem\Util\lockfile(array('path' => $this->path, 'maxCount' => 3));
-		Problem\Util\lockfile(array('path' => $this->path, 'maxCount' => 3));
-		Problem\Util\lockfile(array('path' => $this->path, 'maxCount' => 3));
-		Problem\Util\lockfile(array('path' => $this->path, 'maxCount' => 3));
+		Problem\Util\LockFile::test(array('path' => $this->path, 'maxCount' => 3));
+		Problem\Util\LockFile::test(array('path' => $this->path, 'maxCount' => 3));
+		Problem\Util\LockFile::test(array('path' => $this->path, 'maxCount' => 3));
+		Problem\Util\LockFile::test(array('path' => $this->path, 'maxCount' => 3));
 	}
 
 	function testInvalidCountValue() {
 		$this->setExpectedException('UnexpectedValueException',
 			'Lock file count is not numeric');
-		Problem\Util\lockfile(array('path' => $this->path, 'maxCount' => 3));
+		Problem\Util\LockFile::test(array('path' => $this->path, 'maxCount' => 3));
 		file_put_contents($this->path, 'a');
-		Problem\Util\lockfile(array('path' => $this->path, 'maxCount' => 3));
+		Problem\Util\LockFile::test(array('path' => $this->path, 'maxCount' => 3));
 	}
 
 }
